9e53a04e5a2a308f5aa8f21b7b9295ee03792120
aubastet <alexan182896@gmail.com>
2023-11-02 22:19:22 -0500

    continuacion

---
+++ _partials/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300;400;500;600;700;800;900&family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+      rel="stylesheet"
+    />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+  </head>
+    <link rel="stylesheet" href="./assets/css/layouts.min.css?v=1.0">
+    <!-- Home's stylesheet -->
+    <link rel="stylesheet" href="./assets/css/home.min.css?v=1.0">
+    <link rel="shortcut icon" href="./assets/img/favicon.ico" type="image/x-icon">
+
+    <title> | </title>
+</head>
+<body>
+    <!---inicio-banner-home-->
+    <div class="banner-container">
+      <div class="banner"></div>
+      <nav class="navigation">
+        <div class="nav_content">
+          <img
+            class="logo_web_sc"
+            src="../santa_cecilia/assets/img/logo_web_santa_cecilia.png"
+            alt=""
+          />
+          <a href="#">Home</a>
+          <a href="#">Nosotros</a>
+          <a href="#">Catálogo</a>
+          <a href="#">Proyectos</a>
+          <a href="#">Clientes</a>
+          <a href="#">Blog</a>
+          <a href="#">Contacto</a>
+        </div>
+        <div class="redes_container">
+          <p class="redes_home ">ASK FOR A DATE</p>
+        
+        
+            <div class="redes-sociales">
+        <div class="redes_sociales_icon">
+                <a href="#"><img src="../santa_cecilia/assets/img/instagram.png" alt="Facebook" /></a>
+                <a href="#"><img src="../santa_cecilia/assets/img/facebook.png" alt="Twitter" /></a> 
+                <a href="#"><img src="../santa_cecilia/assets/img/facebook.png" alt="Twitter" /></a> 
+                <a href="#"><img src="../santa_cecilia/assets/img/facebook.png" alt="Twitter" /></a> 
+                <a href="#"><img src="../santa_cecilia/assets/img/facebook.png" alt="Twitter" /></a> 
+                <a href="#"><img src="../santa_cecilia/assets/img/facebook.png" alt="Twitter" /></a> 
+      </div>
+    
+        </div>
+        </div>
+      </nav>
+      <!---->
+
+<body>
+    <main>
